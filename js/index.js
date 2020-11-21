c8899ed2f59c80fcd207c350f6d9519b22de7d9f
clovisdanielcosta <clovisdanielcosta@gmail.com>
2020-11-21 03:59:37 -0300

    Adjustments in css files

---
+++ js/index.js
@@ -0,0 +1,26 @@
+var vid = document.getElementById("myVideo");
+var priv = document.getElementById("privacy");
+
+function openPlay() { 
+    vid.style.display = "none";
+}
+
+function enableAutoplay() { 
+    priv.style.display = "none";
+    vid.style.display = "block";
+    vid.play();
+    disableAutoplay();  
+}
+function disableMute() { 
+    vid.style.display = "none";
+    vid.muted = false;
+    enableAutoplay();
+
+}
+
+function disableAutoplay() { 
+    setTimeout( () => {
+        vid.style.display = "none";
+        window.location = "/open-page.html"
+    }, 5000)  
+} 
